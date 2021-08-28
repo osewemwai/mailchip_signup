fc1bcb0d0fb65a6a57d62738c51cb29d2ac1c6e7
somwai <>
2021-08-28 17:03:37 +0300

    Bug fix

--- app.js
+++ app.js
@@ -56,6 +56,12 @@ app.post("/", (req, res) => {
   run();
 });
 
+// async function run() {
+//   const response = await mailchimp.ping.get();
+//   console.log(response);
+// }
+// run();
+
 app.listen(port, (req, res) => {
   console.log("App listening to port :" + port);
 });
